18b400529e93c48b937db8f491ecc66a185d6eb7
Steve Kim <stevenkim@dev1507.prn1.facebook.com>
2013-12-09 21:39:54 -0800

    fixing up script
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Task ID: #
    
    Blame Rev:

--- sync.sh
+++ sync.sh
@@ -11,10 +11,7 @@ backup_dir=~/dotfiles_backup
 mkdir -p $backup_dir
 
 for file in $files; do
-  if [ -e ~/.$file ]
-  then
-    mv ~/.$file ~/dotfiles_backup/.$file
-    ln -s ~/dotfiles/.$file ~/.$file
-  fi
+  mv ~/.$file ~/dotfiles_backup/.$file
+  ln -s ~/dotfiles/.$file ~/.$file
 done
 
